5579dd76e3774e715eb63f09de1ef554effaba7f
ashrin <ashrin@yahoo.com>
2019-09-29 20:57:49 -0400

    new api changes

--- src/actions/DedupeActions.js
+++ src/actions/DedupeActions.js
@@ -1,4 +1,13 @@
-import { AFTER_MERGE_SAVE, SELECTED_ROW_DATA, SHOW_MODAL } from '.';
+import {
+  AFTER_MERGE_SAVE,
+  SELECTED_ROW_DATA,
+  SHOW_MODAL,
+  GLOBAL_RESPONSE,
+  FETCH_TABLE_DATA,
+  FETCH_TABLE_DATA_PERCENTAGE,
+  INITIAL_TRANSACTION,
+  CURRENT_VERSION
+} from '.';
 
 let actions = {
   setShowModal: value => ({
@@ -12,7 +21,115 @@ let actions = {
   afterMergeSave: finalData => ({
     type: AFTER_MERGE_SAVE,
     data: finalData
-  })
+  }),
+  globalResponse: data => ({
+    type: GLOBAL_RESPONSE,
+    data: data
+  }),
+  postTransaction: (body) => {
+    return function (dispatch) {
+
+      fetch('https://otobots.otomashen.com:6969/dedupe/postTransaction', {
+        method: 'POST',
+        body
+      })
+        .then((data) => (data.json()))
+       
+        .then((data) => {
+
+          dispatch({
+            type: INITIAL_TRANSACTION,
+            initialTransaction: (
+              data
+            )
+          })
+          getTransactionState(data, dispatch);
+
+        })
+       
+        
+    };
+  },
+
+
+updateTransaction: (body, tid) => {
+  
+  return function (dispatch) {
+    let form = new FormData();
+    form.append('transactionId', tid.transactionId);
+    form.append('action', body);
+    form.append('version', tid.version);
+   // console.log("form",form)
+     //console.log("form",form)
+
+    fetch('https://otobots.otomashen.com:6969/dedupe/updateTransactionState', {
+      method: 'POST',
+      body: form
+    })
+      .then((data) => (data.json()))
+      .then((data) =>{
+        console.log("response", data)
+      dispatch({
+        type: INITIAL_TRANSACTION,
+            initialTransaction: (
+              data
+            )
+      })
+     getTransactionState(data, dispatch)
+    }
+      )
+     
+  };
+}
 };
 
+
+
+export const getTransactionState = (data, dispatch) => {
+  const fd = new FormData();
+  fd.append('transactionId', data.transactionId)
+  fd.append('version', data.version)
+  fetch('https://otobots.otomashen.com:6969/dedupe/getTransactionState', {
+    method: 'POST',
+    body: fd
+  }).then((response) => response.json())
+   .then((response) => {
+       const currentIndex = (
+        response.message &&
+        response.message.dedupeFunction &&
+        response.message.dedupeFunction[0] &&
+        response.message.dedupeFunction[0].currentIndex || 0
+      )
+      const numberOfRecords = response.message && response.message.numberOfRecords || 1
+ 
+      if (!(response.message && response.message.hasOwnProperty('completed') && response.message.completed)) {
+        // Call API again if transaction is not completed after 1 sec of API response.
+        setTimeout(() => {
+          getTransactionState(data, dispatch);
+        }, 200)
+
+      } else {
+        dispatch({
+          type: FETCH_TABLE_DATA,
+          dedupeData: (
+            response.message &&
+            response.message.dedupeData &&
+            response.message.dedupeData[data.version] &&
+            response.message.dedupeData[data.version].dedupeJson ||
+            []
+          )
+        })
+
+      }
+
+       dispatch({
+        type: FETCH_TABLE_DATA_PERCENTAGE,
+        percentage: (
+          currentIndex / numberOfRecords
+        ) * 100
+      }); 
+
+    })
+
+}
 export default actions;
