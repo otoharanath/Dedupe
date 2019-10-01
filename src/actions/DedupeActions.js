e9ef5507fb14ee2922779562371ffbed58738adf
ashrin <ashrin@yahoo.com>
2019-09-30 22:16:25 -0400

    undo redo and download

--- src/actions/DedupeActions.js
+++ src/actions/DedupeActions.js
@@ -26,6 +26,10 @@ let actions = {
     type: GLOBAL_RESPONSE,
     data: data
   }),
+  setCurrentVersion: data => ({
+    type: INITIAL_TRANSACTION,
+    initialTransaction: data
+  }),
   postTransaction: (body) => {
     return function (dispatch) {
 
@@ -50,7 +54,61 @@ let actions = {
         
     };
   },
+  getTransactionStateUndoRedo : (data) => {
+    return function (dispatch) {
+    const fd = new FormData();
+    fd.append('transactionId', data.transactionId)
+    fd.append('version', data.version)
+    fetch('https://otobots.otomashen.com:6969/dedupe/getTransactionState', {
+      method: 'POST',
+      body: fd
+    }).then((response) => response.json())
+     .then((response) => {
+         const currentIndex = (
+          response.message &&
+          response.message.dedupeFunction &&
+          response.message.dedupeFunction[0] &&
+          response.message.dedupeFunction[0].currentIndex || 0
+        )
+        const numberOfRecords = response.message && response.message.numberOfRecords || 1
+   
+        if (!(response.message && response.message.hasOwnProperty('completed') && response.message.completed)) {
+          // Call API again if transaction is not completed after 1 sec of API response.
+          setTimeout(() => {
+            getTransactionState(data, dispatch);
+          }, 200)
+  
+        } else {
+          dispatch({
+            type: FETCH_TABLE_DATA,
+            dedupeData: (
+              response.message &&
+              response.message.dedupeData &&
+              response.message.dedupeData[data.version] &&
+              response.message.dedupeData[data.version].dedupeJson ||
+              []
+            )
+          })
+          dispatch({
+            type: INITIAL_TRANSACTION,
+                initialTransaction: (
+                  data
+                )
+          })
+  
+        }
+  
+         dispatch({
+          type: FETCH_TABLE_DATA_PERCENTAGE,
+          percentage: (
+            currentIndex / numberOfRecords
+          ) * 100
+        }); 
+      })
+    }
+    },
 
+    
 
 updateTransaction: (body, tid) => {
   
@@ -72,6 +130,12 @@ updateTransaction: (body, tid) => {
       dispatch({
         type: INITIAL_TRANSACTION,
             initialTransaction: (
+              data
+            )
+      })
+      dispatch({
+        type: CURRENT_VERSION,
+            currentVersion: (
               data
             )
       })
